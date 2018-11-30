3de7d7fc741804f04219ccc002abc10c18c0c0cd
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2018-11-30 14:06:44 -0500

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,36 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import Img from "gatsby-image"
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
-const IndexPage = () => (
+import Typography from '@material-ui/core/Typography';
+
+import 'typeface-bad-script';
+import 'typeface-roboto';
+
+const IndexPage = ({data}) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Img style={{ position: "absolute", left: 0, top: 0, width: "100%", height: "100%", zIndex: "-1"}} fixed={data.file.childImageSharp.fixed}/>
+    <div style = {{marginTop: 100, textAlign: 'center'}}>
+      <Typography variant="h2" style={{color: "#FFF", fontFamily: "Bad Script"}}>Hello World!</Typography>
+      <br/>
+      <Typography variant="h6" style={{color: '#FFF'}}>Lorem ullamco incididunt aute in tempor.</Typography>
+      <Typography variant="h6" style={{color: '#FFF'}}>Culpa incididunt reprehenderit occaecat do pariatur adipisicing magna.</Typography>
+    </div>  
+  </Layout>   
 )
+
+export const imgQuery = graphql`
+  query {
+    file(relativePath: { eq: "background.jpg" }) { 
+      childImageSharp {
+        fixed(width: 2048) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
