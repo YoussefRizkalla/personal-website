8d4f5dc7bc4e229a939c6a7a88db957b64e9fce7
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2018-12-01 14:42:40 -0500

    Add contact info to home page

---
+++ src/components/contact.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import IconButton from '@material-ui/core/IconButton'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const styles = theme => ({
+  paper : {
+    margin: 50,
+    marginBottom: 0,
+    backgroundColor: 'transparent',
+    [theme.breakpoints.up('xl')]: {
+      margin: 250,
+      marginBottom: 0,
+    },
+    [theme.breakpoints.down('xs')]:{
+      margin: 0
+    }
+  },
+})
+
+class Contact extends React.Component {
+  render() {
+    const { classes } = this.props
+    return (
+      <Paper className={classes.paper}>
+      <Typography variant="h4"  style={{color: '#FFF'}}>Contact Me</Typography>
+      <a href='https://github.com/JohnWinter13' target="_blank" rel="noopener noreferrer">   
+          <IconButton aria-label="View Github" style={{color: '#FFF'}}>     
+          <FontAwesomeIcon icon={faGithub} />
+          </IconButton>
+      </a>
+      <a href='https://www.linkedin.com/in/youssef-rizkalla' target="_blank" rel="noopener noreferrer">   
+          <IconButton aria-label="View Linkedin" style={{color: '#FFF'}}>
+          <FontAwesomeIcon icon={faLinkedin} />
+          </IconButton>
+      </a>
+      <a href='mailto:rizkally@mcmaster.ca' target="_blank" rel="noopener noreferrer">        
+          <IconButton aria-label="Email Me" style={{color: '#FFF'}}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          </IconButton>
+      </a>
+      </Paper>
+    )
+  }
+}
+
+Contact.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Contact)
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,22 @@ import { graphql } from 'gatsby'
 import Img from "gatsby-image"
 
 import Layout from '../components/layout'
+import Contact from '../components/contact'
 
 import Typography from '@material-ui/core/Typography';
 
 import 'typeface-bad-script';
 import 'typeface-roboto';
 
-const IndexPage = ({data}) => (
+const IndexPage = ({data}, theme) => (
   <Layout>
     <Img style={{ position: "absolute", left: 0, top: 0, width: "100%", height: "100%", zIndex: "-1"}} fixed={data.file.childImageSharp.fixed}/>
-    <div style = {{marginTop: 100, textAlign: 'center'}}>
+    <div style = {{marginTop: 100, textAlign: 'center',}}>
       <Typography variant="h2" style={{color: "#FFF", fontFamily: "Bad Script"}}>Hello World!</Typography>
       <br/>
-      <Typography variant="h6" style={{color: '#FFF'}}>Lorem ullamco incididunt aute in tempor.</Typography>
-      <Typography variant="h6" style={{color: '#FFF'}}>Culpa incididunt reprehenderit occaecat do pariatur adipisicing magna.</Typography>
+      <Typography variant="h6" style={{color: '#FFF'}}>I&#39;m a computer science student at McMaster University.</Typography>
+      <Typography variant="h6" style={{color: '#FFF'}}>Passionate about software development, computing and problem solving.</Typography>
+      <Contact></Contact>
     </div>  
   </Layout>   
 )
