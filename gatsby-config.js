5a6668b26506e1388753250d78416c157dd383e7
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2019-02-16 14:18:15 -0500

    Update npm packages and SSR

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,6 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-plugin-offline',
     'gatsby-plugin-sitemap',
-    'gatsby-plugin-material-ui',
     `gatsby-plugin-styled-components`,
   ],
 }
