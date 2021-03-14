73ea5ea55e1d04756d4767c5ff8f1a3f0d4fc975
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2021-03-13 20:13:29 -0500

    Tailwind css setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        // eslint-disable-next-line global-require
+        postCssPlugins: [require('tailwindcss')],
+      },
+    },
+  ],
+};
