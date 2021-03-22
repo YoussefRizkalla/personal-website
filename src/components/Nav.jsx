30dc05ac9387751083a1edd34109934f7a49b05e
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2021-03-22 00:03:55 -0400

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Button from './Button';
 
 const Nav = () => (
-  <nav className="bg-gray-800">
+  <nav className="bg-blue-500">
     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div className="relative flex items-center justify-between h-16">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -59,30 +60,10 @@ const Nav = () => (
           </div>
           <div className="hidden sm:block sm:ml-6">
             <div className="flex space-x-4">
-              <a
-                href="#"
-                className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Home
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Projects
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Blog
-              </a>
-              <a
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Contact Me
-              </a>
+              <Button>Home</Button>
+              <Button>Projects</Button>
+              <Button>Blog</Button>
+              <Button>Contact Me</Button>
             </div>
           </div>
         </div>
