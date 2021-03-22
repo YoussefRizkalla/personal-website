a07f4d2dd8b316b372485952380f5503be60073b
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2021-03-22 00:03:49 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Footer';
 import Nav from './Nav';
 
 const Layout = ({ children }) => (
-  <div>
+  <div className="bg-blue-200">
     <Nav />
     {children}
+    <Footer />
   </div>
 );
 
