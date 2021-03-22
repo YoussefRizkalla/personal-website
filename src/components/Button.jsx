0923feb4dc516eb9b42db99abf004a12c4097d8b
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2021-03-22 00:03:27 -0400

    Button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ children }) => (
   <button
-    className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none active:bg-blue-500"
+    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none active:bg-blue-400"
     type="button"
   >
     {children}
