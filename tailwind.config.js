b5c752107feaea225b40d74a837d16ed62dc0c0d
Youssef Rizkalla <32617102+JohnWinter13@users.noreply.github.com>
2021-03-22 20:26:38 -0400

    Add Bad Script font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      fancy: ['Bad Script', 'cursive'],
+    },
   },
   variants: {
     extend: {
